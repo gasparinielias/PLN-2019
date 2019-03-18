2e2eb68f150eafc368aa926fb0611443ba4ede1d
Elías Gasparini <gasparinielias@gmail.com>
2019-03-18 19:28:27 -0300

    Implemented NGramGenerator.__init__() and generate_sent().

--- languagemodeling/ngram_generator.py
+++ languagemodeling/ngram_generator.py
@@ -12,17 +12,36 @@ class NGramGenerator(object):
 
         # compute the probabilities
         probs = defaultdict(dict)
-        # WORK HERE!!
 
+        for ngram in model._count.keys():
+            if len(ngram) == self._n:
+               probs[ngram[:-1]][ngram[-1]] = model.cond_prob(ngram[-1], ngram[:-1])
         self._probs = dict(probs)
 
         # sort in descending order for efficient sampling
         self._sorted_probs = sorted_probs = {}
-        # WORK HERE!!
+        for ngram, probs in self._probs.items():
+            sorted_probs[ngram] = {}
+            for key, value in sorted(probs.items(), key=lambda item: (-item[1], item[0])):
+                sorted_probs[ngram][key] = value
+        self._sorted_probs = sorted_probs
 
     def generate_sent(self):
         """Randomly generate a sentence."""
-        # WORK HERE!!
+        sent = ['<s>'] * (self._n - 1)
+        while '</s>' not in sent:
+            u = random.random()
+            last_ngram = tuple(sent[len(sent) - self._n + 1:])
+            prob_dict = self._sorted_probs[last_ngram]
+            acum_prob = 0
+            for word, prob in prob_dict.items():
+                acum_prob += prob
+                if u < acum_prob:
+                    sent += [word]
+                    break
+
+        return sent
+
 
     def generate_token(self, prev_tokens=None):
         """Randomly generate a token, given prev_tokens.
